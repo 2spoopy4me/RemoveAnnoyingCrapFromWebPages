2ed233aaf5a0390a86534685e9d820d6c9874b22
Luke <lukeendres@Lukes-MacBook-Air.local>
2017-05-20 15:42:50 -0500

    1st commit

---
+++ VanillaYoutube.js
@@ -0,0 +1,8 @@
+/* Strips a youtube page down to a minimalistic design.
+
+Cool for people with OCD or who prefer barebones 'no strings attatched' examples of raw content. */	
+
+$('#yt-masthead-container').remove();
+$('#watch7-sidebar-contents').remove();
+$('#watch-discussion').remove();
+$('#watch-discussion').remove();
